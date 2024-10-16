99787bb7ba93e02ac2497c7fb67d040a09116dd4
Lukman <lukmanmakhaev@gmail.com>
2024-10-16 23:11:08 +0300

    Initial Commit

---
+++ AnsarTest/AnsarTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnsarTestApp.swift
+//  AnsarTest
+//
+//  Created by Lukman Makhaev on 16.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnsarTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
